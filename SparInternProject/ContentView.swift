bda715ad3eba078a38ae44f994aea0e375d82ccd
Gorev <slavny45@gmail.com>
2024-08-17 11:19:18 +0300

    Initial Commit

---
+++ SparInternProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SparInternProject
+//
+//  Created by Вячеслав Горев on 17/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SparInternProject/SparInternProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparInternProjectApp.swift
+//  SparInternProject
+//
+//  Created by Вячеслав Горев on 17/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct SparInternProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
